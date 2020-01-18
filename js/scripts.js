f903cedc0e916dc29b9f132646fffb4543efb5b6
jd <duverseaujohnny21@gmail.com>
2020-01-17 16:35:52 -0800

    add if, else if

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
     $("form#group").submit(function(event) {
-        var side1 = parseInt($("select#add1").val());
-        var side2 = parseInt($("select#add2").val());
+        var side1 = parseInt($("#add1").val());
+        var side2 = parseInt($("#add2").val());
         var side3 = parseInt($("#add3").val());
-        var side4 = parseInt($("select#add4").val());
-        var side5 = parseInt($("select#add5").val());
+        var side4 = parseInt($("#add4").val());
+        var side5 = parseInt($("#add5").val());
 
-        var addUp = ()
+        var addUp = (side1 + side2 + side3 + side4 + side5 )
 
-        if ( side1 === side2 && side2 === side3 && side4 === side5)  {
+        if ( addUp >= 5 && addUp <= 10) {
             $("#csharp").show();
+        } else if ( addUp >= 6 || addUp <= 10) {
+            $("#python").show();
         }
+
+
+
+
 
 event.preventDefault();
     });
